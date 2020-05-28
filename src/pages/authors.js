b2aa1c2b2ee94616f44194e95f945967b7f801bc
ali neer <ali@alineer.com>
2020-05-28 14:31:57 -0400

    authors pages

--- src/pages/authors.js
+++ src/pages/authors.js
@@ -2,24 +2,24 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 
-import { Layout, TagCard, Pagination } from '../components/common'
+import { Layout, AuthorCard, Pagination } from '../components/common'
 import { MetaData } from '../components/common/meta'
 
-const Tags = ({ data, location, pageContext }) => {
-    let tags = data.allGhostTag.edges
+const Authors = ({ data, location, pageContext }) => {
+    let authors = data.allGhostAuthor.edges
     return (
         <>
         <MetaData location={location} />
         <Layout>
             <div className="container">
                 <section className="post-feed">
-                {
-                    tags.map(({ node }) => (
-                        // The tag below includes the markup for each post - components/common/PostCard.js
-                        <TagCard key={node.id} tag={node} />
+                    {
+                        authors.map(({ node }) => (
+                            // The tag below includes the markup for each post - components/common/PostCard.js
+                            <AuthorCard key={node.id} author={node} />
+                            )
                         )
-                    )
-                }
+                    }
                 </section>
                 <Pagination pageContext={pageContext} />
             </div>
@@ -28,9 +28,9 @@ const Tags = ({ data, location, pageContext }) => {
     )
 }
 
-Tags.propTypes = {
+Authors.propTypes = {
     data: PropTypes.shape({
-        allGhostTag: PropTypes.object.isRequired,
+        allGhostAuthor: PropTypes.object.isRequired,
     }).isRequired,
     location: PropTypes.shape({
         pathname: PropTypes.string.isRequired
@@ -38,14 +38,14 @@ Tags.propTypes = {
     pageContext: PropTypes.object
 }
 
-export default Tags
+export default Authors
 
 export const pageQuery = graphql`
     query{
-        allGhostTag(sort:{ order: DESC, fields: postCount }){
+        allGhostAuthor(sort:{ order: DESC, fields: postCount }){
             edges{
                 node {
-                    ...GhostTagFields
+                    ...GhostAuthorFields
                 }
             }
         }
